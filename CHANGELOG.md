ba34c534af59c7cebc89ca11e56eb8c31a267bbc
sayampradhan <sayampradhan07@outlook.com>
2026-03-20 00:17:42 +0530

    Added CHANGELOG.md to log future changes

---
+++ CHANGELOG.md
@@ -0,0 +1,4 @@
+# Changelog
+
+## 0.1.0 - 2026-03-20
+- Initial release
--- patlab/__init__.py
+++ patlab/__init__.py
@@ -1,7 +1,6 @@
-# __init__.py
-
 from .basic import square
 from .pyramid import make as pyramid
 from .right_triangle import make as right_triangle
 
+__version__ = "0.1.0"
 __all__ = ["square", "right_triangle", "pyramid"]
