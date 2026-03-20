b171d9a4b6e58f02997d723c1922bd776fb0882c
sayampradhan <sayampradhan07@outlook.com>
2026-03-21 04:40:15 +0530

    code: fixed imports

--- patlab/__init__.py
+++ patlab/__init__.py
@@ -1,5 +1,5 @@
 from .square import square
-from .pyramid import pyramid
-from .right_triangle import make as right_triangle
+from .pyramid import make as pyramid
+from .right_triangle import right_triangle
 
 __all__ = ["square", "right_triangle", "pyramid"]
