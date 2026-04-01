71459c150692fc5d9c0c8d54bf903ba0e7325ac7
sayampradhan <sayampradhan07@outlook.com>
2026-04-01 20:31:13 +0530

    code: added rectangle and release 0.2.14

--- patlab/__init__.py
+++ patlab/__init__.py
@@ -3,5 +3,6 @@ from .pyramid import pyramid
 from .pyramid import numeric_pyramid
 from .right_triangle import right_triangle
 from .diamond import diamond
+from .rectangle import rectangle
 
-__all__ = ["square", "right_triangle", "pyramid", "numeric_pyramid", "diamond"]
+__all__ = ["square", "right_triangle", "pyramid", "numeric_pyramid", "diamond", "rectangle"]
